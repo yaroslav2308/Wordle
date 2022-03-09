2932ce5fb345d6de5b150dcc6a7d5e535e442055
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-03-09 23:40:23 +0300

    Add full functionality and reload word button

--- Wordle/Core/BoardViewController.swift
+++ Wordle/Core/BoardViewController.swift
@@ -9,16 +9,14 @@ import UIKit
 
 protocol BoardViewControllerDatasource: AnyObject {
     var currentGuesses: [[Character?]] { get }
+    func boxColor(at indexPath: IndexPath) -> UIColor?
 }
 
 class BoardViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    var guesses: [[Character?]] = Array(
-        repeating: Array(repeating: nil, count: 5),
-        count: 6
-    )
+    weak var datasource: BoardViewControllerDatasource?
     
-    let collectionView: UICollectionView = {
+    private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumInteritemSpacing = 4
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
@@ -41,15 +39,20 @@ class BoardViewController: UIViewController, UICollectionViewDelegate, UICollect
         ])
     }
     
+    public func reloadData() {
+        collectionView.reloadData()
+    }
+    
 }
 
 extension BoardViewController {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return guesses.count
+        return datasource?.currentGuesses.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let guesses = datasource?.currentGuesses ?? []
         return guesses[section].count
     }
     
@@ -57,9 +60,14 @@ extension BoardViewController {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KeyCell.identifier, for: indexPath) as? KeyCell else {
             fatalError()
         }
-        cell.backgroundColor = nil
+        cell.backgroundColor = datasource?.boxColor(at: indexPath)
         cell.layer.borderWidth = 1
         cell.layer.borderColor = UIColor.systemGray3.cgColor
+        
+        let guesses = datasource?.currentGuesses ?? []
+        if let letter = guesses[indexPath.section][indexPath.row] {
+            cell.configure(with: letter)
+        }
         return cell
     }
     
@@ -82,7 +90,6 @@ extension BoardViewController {
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //
     }
 }
 
--- Wordle/Core/ViewController.swift
+++ Wordle/Core/ViewController.swift
@@ -9,14 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let answers = ["lunch", "chart", "scene", "graph", "doubt", "guide", "winds", "block", "grain", "smoke", "mixed", "games", "wagon", "sweet", "topic", "extra", "plate", "title", "knife", "fence", "falls", "cloud", "wheat", "plays", "enter", "broad", "steam", "atoms", "press", "lying", "basis", "clock", "taste", "grows", "thank", "storm", "agree", "brain", "track", "smile", "funny", "beach", "stock", "hurry", "saved", "sorry", "giant", "trail", "offer", "ought", "rough"]
+    
+    var answer = ""
+    private var guesses: [[Character?]] = Array(
+        repeating: Array(repeating: nil, count: 5),
+        count: 6
+    )
+    
     let keyboardVC = KeyboardViewController()
     let boardVC = BoardViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        answer = answers.randomElement() ?? "yarik"
         view.backgroundColor = .systemGray6
         
         addChildren()
+        configureReloadButton()
     }
     
     private func addChildren() {
@@ -29,9 +39,28 @@ class ViewController: UIViewController {
         addChild(boardVC)
         boardVC.didMove(toParent: self)
         boardVC.view.translatesAutoresizingMaskIntoConstraints = false
+        boardVC.datasource = self
         view.addSubview(boardVC.view)
         
         addConstraints()
+    }
+    
+    private func configureReloadButton() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .refresh,
+            target: self,
+            action: #selector(didTappedReloadButton))
+        self.navigationItem.rightBarButtonItem?.tintColor = .white
+    }
+    
+    @objc private func didTappedReloadButton() {
+        for i in 0..<guesses.count {
+            for j in 0..<guesses[i].count {
+                guesses[i][j] = nil
+            }
+        }
+        boardVC.reloadData()
+        answer = answers.randomElement() ?? "yarik"
     }
     
     func addConstraints() {
@@ -53,7 +82,55 @@ class ViewController: UIViewController {
 
 extension ViewController: KeyboardViewControllerDelegate {
     func keyboardViewController(_ vc: KeyboardViewController, didTapKey letter: Character) {
-        print(letter)
+        // update guesses
+        
+        var stop = false
+        
+        for i in 0..<guesses.count {
+            for j in 0..<guesses[i].count {
+                if guesses[i][j] == nil {
+                    print(letter)
+                    guesses[i][j] = letter
+                    stop = true
+                    break
+                }
+            }
+            
+            if stop {
+                break
+            }
+        }
+        
+        boardVC.reloadData()
     }
 }
+
+extension ViewController: BoardViewControllerDatasource {
+    var currentGuesses: [[Character?]] {
+        return guesses
+    }
+    
+    func boxColor(at indexPath: IndexPath) -> UIColor? {
+        let rowIndex = indexPath.section
+        
+        let count = guesses[rowIndex].compactMap({ $0 }).count
+        guard count == 5 else {
+            return nil
+        }
+        
+        let indexedAnswer = Array(answer)
+        
+        guard let letter = guesses[indexPath.section][indexPath.row], indexedAnswer.contains(letter) else {
+            return nil
+        }
+        
+        if indexedAnswer[indexPath.row] == letter {
+            return .systemGreen
+        }
+        
+        
+        return .systemOrange
+    }
+}
+
 
